cc75af7384dc86bbc5ea3a0c32c03438d306a4fe
User <user@bk0010.myftp.org>
2014-06-13 23:24:21 +0400

    Причёсано всё

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace gitexample
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Требуется переменная конструктора.
@@ -28,26 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnClick = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnClick
             // 
-            this.button1.Location = new System.Drawing.Point(65, 86);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(157, 82);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnClick.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnClick.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnClick.Location = new System.Drawing.Point(0, 0);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(124, 61);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Нажми";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(124, 61);
+            this.Controls.Add(this.btnClick);
+            this.Name = "frmMain";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -55,7 +57,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClick;
     }
 }
 
